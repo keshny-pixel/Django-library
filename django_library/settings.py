c8b8acd4f48ade6fae549d800a9f4cef358acd6e
keshny <keshnyseetahul@gmail.com>
2023-01-31 00:23:28 +0400

    Create templates folder and added base.html

--- django_library/settings.py
+++ django_library/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'books.apps.BooksConfig',
+    'crispy_bootstrap5',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
